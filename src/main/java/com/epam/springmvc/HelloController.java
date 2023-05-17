9f6ee14660b85dd7a55c7bfea18467339d465989
Anzhalika Baradzina <anzhalika_baradzina@epam.com>
2023-05-17 23:26:16 +0200

    springmvc

---
+++ src/main/java/com/epam/springmvc/HelloController.java
@@ -0,0 +1,13 @@
+package com.epam.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello_world")
+    public String hi() {
+        return "hello_world";
+    }
+}
